c12a69c3c0e4d29d28feaefdbf8110a16321b81a
Lauren laptop <phiipaswa01@gmail.com>
2024-01-24 00:02:15 +0100

    Advanced v1

--- reg4.py
+++ reg4.py
@@ -2,25 +2,18 @@ import tkinter as tk
 import customtkinter as ctk
 from customtkinter import CTk, CTkLabel, CTkEntry, CTkCheckBox, CTkButton
 from PIL import ImageTk, Image
+# from PIL.Image import ANTIALIAS
 
 def on_entry_click(entry, default_text):
     if entry.get() == default_text:
         entry.delete(0, tk.END)
-        entry.insert(0, '')
-        entry.configure(font=('normal', 10))  # Set font size to 10
-
-def on_entry_typing(event, entry, default_text):
-    current_text = entry.get()
-    if current_text == default_text or current_text == '':
-        entry.delete(0, tk.END)
-        entry.configure(font=('italic', 10))  # Italicize text, set font size to 10
-    else:
-        entry.configure(font=('normal', 14))  # Set font size to 14 for user's input
+        entry.insert(0, default_text)
+        entry.configure(font=('italic', 10))  # Italicize text
 
 def on_focus_out(entry, default_text):
     if entry.get() == '':
         entry.insert(0, default_text)
-        entry.configure(font=('normal', 10))  # Set font size to 10
+        entry.configure(font=('normal', 14))  # Set back to normal font
 
 def getvals():
     print("Accepted too, of course")
@@ -32,7 +25,11 @@ root.geometry(f"{root_width}x{root_height}")
 root.title("Parking Management System")
 
 # Form label
-img1 = ImageTk.PhotoImage(Image.open("logo.png").resize((800, 800)))
+img = Image.open("logo2.png")
+# img = img.resize((200, 200))  # Resize the image
+# img1 = ctk.CTkImage(ImageTk.PhotoImage(img))
+# img1 = ImageTk.PhotoImage(Image.open("logo.png"))
+img1 = ImageTk.PhotoImage(Image.open("logo.png").resize((800,800)))
 Label_frame = CTkLabel(root, text="Registration Form", font=("ar", 15, "bold"), image=img1)
 Label_frame.pack(padx=20, pady=20, fill=tk.BOTH, expand=True)
 
@@ -54,7 +51,6 @@ for i, group in enumerate(groups):
     entry.configure(font=('italic', 10))  # Initial font style
     entry.pack(padx=5, pady=5, fill=tk.X, expand=True)
     entry.bind('<FocusIn>', lambda event, entry=entry, default_text=f"{field_labels[i]} goes here": on_entry_click(entry, default_text))
-    entry.bind('<Key>', lambda event, entry=entry, default_text=f"{field_labels[i]} goes here": on_entry_typing(event, entry, default_text))
     entry.bind('<FocusOut>', lambda event, entry=entry, default_text=f"{field_labels[i]} goes here": on_focus_out(entry, default_text))
 
 # Creating Checkbox
